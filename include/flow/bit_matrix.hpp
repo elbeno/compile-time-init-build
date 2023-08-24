d600578436c59ff60ded8386b47dc561684e12da
Ben Deane <ben@elbeno.com>
2023-08-24 07:15:00 -0600

    wip

---
+++ include/flow/bit_matrix.hpp
@@ -0,0 +1,332 @@
+#pragma once
+
+#include <fmt/format.h>
+
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <concepts>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <ostream>
+#include <string_view>
+
+namespace flow {
+namespace detail {
+constexpr auto transpose8x8(std::uint64_t x) {
+    std::uint64_t t = (x ^ (x >> 7u)) & 0x00aa'00aa'00aa'00aaull;
+    x ^= t ^ (t << 7u);
+    t = (x ^ (x >> 14u)) & 0x0000'cccc'0000'ccccull;
+    x ^= t ^ (t << 14u);
+    t = (x ^ (x >> 28u)) & 0x0000'0000'f0f0'f0f0ull;
+    x ^= t ^ (t << 28u);
+    return x;
+}
+} // namespace detail
+
+template <std::size_t N> class bit_matrix {
+    using elem_type = std::uint64_t;
+
+    constexpr static auto block_size = 8u;
+    constexpr static auto num_blocks =
+        std::bit_ceil((N + block_size - 1) / block_size);
+
+    constexpr static auto bit = elem_type{1u};
+    constexpr static auto allbits = std::numeric_limits<elem_type>::max();
+    constexpr static elem_type identity_elem = 0x80'40'20'10'08'04'02'01;
+
+    constexpr static auto leftover_rows = N % block_size;
+    constexpr static elem_type bottom_row_mask =
+        (leftover_rows != 0) ? (bit << (leftover_rows * block_size)) - 1
+                             : allbits;
+
+    constexpr static auto leftover_cols = N % block_size;
+    constexpr static elem_type right_column_mask8 =
+        (leftover_cols != 0) ? (bit << leftover_cols) - 1 : 0xffu;
+    constexpr static elem_type right_column_mask =
+        right_column_mask8 | (right_column_mask8 << 8u) |
+        (right_column_mask8 << 16u) | (right_column_mask8 << 24u) |
+        (right_column_mask8 << 32u) | (right_column_mask8 << 40u) |
+        (right_column_mask8 << 48u) | (right_column_mask8 << 56u);
+
+    std::array<elem_type, num_blocks * num_blocks> storage{};
+
+    struct index_t {
+        std::size_t array_index;
+        std::size_t bit_index;
+    };
+
+    constexpr auto set(index_t i, bool value) -> bit_matrix & {
+        auto const [ai, bi] = i;
+        if (value) {
+            storage[ai] |= (bit << bi);
+        } else {
+            storage[ai] &= ~(bit << bi);
+        }
+        return *this;
+    }
+
+    constexpr auto get(index_t i) const -> bool {
+        auto const [ai, bi] = i;
+        return (storage[ai] & (bit << bi)) != 0;
+    }
+
+    struct proxy : index_t {
+        bit_matrix *m;
+        constexpr auto operator=(bool value) -> proxy & {
+            m->set(*this, value);
+            return *this;
+        }
+
+        constexpr explicit(true) operator bool() const { return m->get(*this); }
+    };
+
+    friend constexpr auto bodies_equal(bit_matrix const &lhs,
+                                       bit_matrix const &rhs) -> bool {
+        for (auto i = std::size_t{}; i < num_blocks - 1; ++i) {
+            for (auto j = std::size_t{}; j < num_blocks - 1; ++j) {
+                if (lhs.storage[i * num_blocks + j] !=
+                    rhs.storage[i * num_blocks + j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    friend constexpr auto right_columns_equal(bit_matrix const &lhs,
+                                              bit_matrix const &rhs) -> bool {
+        constexpr auto right_col = num_blocks - 1;
+        for (auto i = std::size_t{}; i < num_blocks - 1; ++i) {
+            if ((lhs.storage[i * num_blocks + right_col] & right_column_mask) !=
+                (rhs.storage[i * num_blocks + right_col] & right_column_mask)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    friend constexpr auto bottom_rows_equal(bit_matrix const &lhs,
+                                            bit_matrix const &rhs) -> bool {
+        constexpr auto bottom_row_start = num_blocks * (num_blocks - 1);
+        for (auto i = std::size_t{}; i < num_blocks - 1; ++i) {
+            if ((lhs.storage[bottom_row_start + i] & bottom_row_mask) !=
+                (rhs.storage[bottom_row_start + i] & bottom_row_mask)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    friend constexpr auto lasts_equal(bit_matrix const &lhs,
+                                      bit_matrix const &rhs) -> bool {
+        constexpr auto last_mask = right_column_mask & bottom_row_mask;
+        return (lhs.storage.back() & last_mask) ==
+               (rhs.storage.back() & last_mask);
+    }
+
+    // friend constexpr auto operator==(bit_matrix const &lhs,
+    //                                  bit_matrix const &rhs) -> bool {
+    //     return bodies_equal(lhs, rhs) and right_columns_equal(lhs, rhs) and
+    //            bottom_rows_equal(lhs, rhs) and lasts_equal(lhs, rhs);
+    // }
+    friend constexpr auto operator==(bit_matrix const &lhs,
+                                     bit_matrix const &rhs) -> bool = default;
+
+    friend constexpr auto operator|(bit_matrix lhs, bit_matrix const &rhs)
+        -> bit_matrix {
+        lhs |= rhs;
+        return lhs;
+    }
+
+    friend constexpr auto operator&(bit_matrix lhs, bit_matrix const &rhs)
+        -> bit_matrix {
+        lhs &= rhs;
+        return lhs;
+    }
+
+    friend constexpr auto operator^(bit_matrix lhs, bit_matrix const &rhs)
+        -> bit_matrix {
+        lhs ^= rhs;
+        return lhs;
+    }
+
+    friend constexpr auto operator~(bit_matrix const &m) -> bit_matrix {
+        bit_matrix r{};
+        std::transform(std::begin(m.storage), std::end(m.storage),
+                       std::begin(r.storage), std::bit_not{});
+        return r;
+    }
+
+    friend constexpr auto transpose(bit_matrix const &m) -> bit_matrix {
+        bit_matrix result{};
+        for (auto i = std::size_t{}; i < num_blocks; ++i) {
+            for (auto j = std::size_t{}; j < num_blocks; ++j) {
+                result.storage[j * num_blocks + i] =
+                    detail::transpose8x8(m.storage[i * num_blocks + j]);
+            }
+        }
+        return result;
+    }
+
+    friend constexpr auto operator+(bit_matrix lhs, bit_matrix const &rhs)
+        -> bit_matrix {
+        lhs += rhs;
+        return lhs;
+    }
+
+    //   constexpr auto operator*=(bit_matrix const &m) -> bit_matrix &
+    //   {
+    //       auto const lhs = *this;
+    //       auto const rhs = transpose(m);
+    //       storage.fill(0);
+    //       for (auto i = std::size_t{}; i < num_rows; ++i) {
+    //           for (auto j = std::size_t{}; j < num_rows; ++j) {
+    //               for (auto k = std::size_t{}; k < num_cols; ++k) {
+    //                   if ((lhs.storage[i * num_cols + k] &
+    //                        rhs.storage[j * num_cols + k]) != 0) {
+    //                       set(i, j);
+    //                       break;
+    //                   }
+    //               }
+    //           }
+    //       }
+    //       return *this;
+    //   }
+    //   friend constexpr auto operator*(bit_matrix lhs, bit_matrix
+    //   const &rhs)
+    //       -> bit_matrix {
+    //       lhs *= rhs;
+    //       return lhs;
+    //   }
+
+    //   friend constexpr auto pow(bit_matrix const &m, std::size_t n)
+    //       -> bit_matrix {
+    //       if (n == 1) {
+    //           return m;
+    //       }
+    //       if (n % 2 == 0) {
+    //           auto const r = pow(m, n / 2);
+    //           return r * r;
+    //       }
+    //       return m * pow(m, n - 1);
+    //   }
+
+    //   friend auto operator<<(std::ostream &os, bit_matrix const &m)
+    //       -> std::ostream & {
+    //       for (auto i = std::size_t{}; i < num_rows; ++i) {
+    //           for (auto j = std::size_t{}; j < num_cols; ++j) {
+    //               auto const e = m.storage[i * num_cols + j];
+    //               for (auto k = std::uint8_t{1}; k != 0; k <<= 1u) {
+    //                   os << (((e & k) == 0) ? '0' : '1');
+    //               }
+    //           }
+    //           os << '\n';
+    //       }
+    //       return os;
+    //   }
+
+  public:
+    constexpr bit_matrix() = default;
+    constexpr bit_matrix(std::string_view v) {
+        auto i = std::size_t{};
+        auto j = std::size_t{};
+        for (auto c : v) {
+            if (c == '1') {
+                set(i, j);
+            }
+            ++i;
+            if (i == N) {
+                i = 0;
+                ++j;
+            }
+        }
+    }
+
+    [[nodiscard]] constexpr static auto identity() -> bit_matrix {
+        bit_matrix m{};
+        for (auto i = std::size_t{}; i < num_blocks; ++i) {
+            m.storage[i * num_blocks + i] = identity_elem;
+        }
+        m.storage.back() &= right_column_mask & bottom_row_mask;
+        return m;
+    }
+
+    [[nodiscard]] constexpr static auto compute_index(std::size_t row,
+                                                      std::size_t col) {
+        auto const block_row_index = row / block_size;
+        auto const block_col_index = col / block_size;
+        auto const ai = block_row_index * num_blocks + block_col_index;
+
+        auto const block_row_subindex = row % block_size;
+        auto const block_col_subindex = col % block_size;
+        auto const bi = block_row_subindex * block_size + block_col_subindex;
+
+        return index_t{ai, bi};
+    }
+
+    [[nodiscard]] constexpr auto index(std::size_t row, std::size_t col) const
+        -> bool {
+        return get(compute_index(row, col));
+    }
+
+    [[nodiscard]] constexpr auto index(std::size_t row, std::size_t col)
+        -> proxy {
+        return {compute_index(row, col), this};
+    }
+
+    constexpr auto set(std::size_t row, std::size_t col, bool value = true)
+        -> bit_matrix & {
+        return set(compute_index(row, col), value);
+    }
+
+    constexpr auto set() -> bit_matrix & {
+        storage.fill(allbits);
+
+        constexpr auto right_col = num_blocks - 1;
+        for (auto i = std::size_t{}; i < num_blocks; ++i) {
+            storage[i * num_blocks + right_col] &= right_column_mask;
+        }
+        constexpr auto bottom_row_start = num_blocks * (num_blocks - 1);
+        for (auto i = std::size_t{}; i < num_blocks; ++i) {
+            storage[bottom_row_start + i] &= bottom_row_mask;
+        }
+
+        return *this;
+    }
+
+    constexpr auto clear() -> bit_matrix & {
+        storage.fill(0u);
+        return *this;
+    }
+
+    constexpr auto operator|=(bit_matrix const &rhs) -> bit_matrix & {
+        std::transform(std::begin(storage), std::end(storage),
+                       std::begin(rhs.storage), std::begin(storage),
+                       std::bit_or{});
+        return *this;
+    }
+
+    constexpr auto operator&=(bit_matrix const &rhs) -> bit_matrix & {
+        std::transform(std::begin(storage), std::end(storage),
+                       std::begin(rhs.storage), std::begin(storage),
+                       std::bit_and{});
+        return *this;
+    }
+
+    constexpr auto operator^=(bit_matrix const &rhs) -> bit_matrix & {
+        std::transform(std::begin(storage), std::end(storage),
+                       std::begin(rhs.storage), std::begin(storage),
+                       std::bit_xor{});
+        return *this;
+    }
+
+    constexpr auto operator+=(bit_matrix const &rhs) -> bit_matrix & {
+        return this->operator^=(rhs);
+    }
+};
+} // namespace flow
---
+++ test/flow/bit_matrix.cpp
@@ -0,0 +1,267 @@
+#include <flow/bit_matrix.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <cstddef>
+#include <string_view>
+
+TEST_CASE("default construct", "[bit matrix]") {
+    flow::bit_matrix<32> m{};
+    static_assert(sizeof(m) == 128);
+}
+
+TEST_CASE("size", "[bit matrix]") {
+    static_assert(sizeof(flow::bit_matrix<1>) == 8);
+    static_assert(sizeof(flow::bit_matrix<2>) == 8);
+    static_assert(sizeof(flow::bit_matrix<8>) == 8);
+    static_assert(sizeof(flow::bit_matrix<9>) == 32);
+    static_assert(sizeof(flow::bit_matrix<16>) == 32);
+    static_assert(sizeof(flow::bit_matrix<17>) == 128);
+}
+
+TEST_CASE("index compute (block 0,0 extrema)", "[bit matrix]") {
+    flow::bit_matrix<16> m{}; // 2x2 block
+    {
+        auto const [ai, bi] = m.compute_index(0, 0);
+        CHECK(ai == 0);
+        CHECK(bi == 0);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(0, 7);
+        CHECK(ai == 0);
+        CHECK(bi == 7);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(7, 0);
+        CHECK(ai == 0);
+        CHECK(bi == 56);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(7, 7);
+        CHECK(ai == 0);
+        CHECK(bi == 63);
+    }
+}
+
+TEST_CASE("index compute (block 0,1 extrema)", "[bit matrix]") {
+    flow::bit_matrix<16> m{}; // 2x2 block
+    {
+        auto const [ai, bi] = m.compute_index(0, 8);
+        CHECK(ai == 1);
+        CHECK(bi == 0);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(0, 15);
+        CHECK(ai == 1);
+        CHECK(bi == 7);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(7, 8);
+        CHECK(ai == 1);
+        CHECK(bi == 56);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(7, 15);
+        CHECK(ai == 1);
+        CHECK(bi == 63);
+    }
+}
+
+TEST_CASE("index compute (block 1,0 extrema)", "[bit matrix]") {
+    flow::bit_matrix<16> m{}; // 2x2 block
+    {
+        auto const [ai, bi] = m.compute_index(8, 0);
+        CHECK(ai == 2);
+        CHECK(bi == 0);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(8, 7);
+        CHECK(ai == 2);
+        CHECK(bi == 7);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(15, 0);
+        CHECK(ai == 2);
+        CHECK(bi == 56);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(15, 7);
+        CHECK(ai == 2);
+        CHECK(bi == 63);
+    }
+}
+
+TEST_CASE("index compute (block 1,1 extrema)", "[bit matrix]") {
+    flow::bit_matrix<16> m{}; // 2x2 block
+    {
+        auto const [ai, bi] = m.compute_index(8, 8);
+        CHECK(ai == 3);
+        CHECK(bi == 0);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(8, 15);
+        CHECK(ai == 3);
+        CHECK(bi == 7);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(15, 8);
+        CHECK(ai == 3);
+        CHECK(bi == 56);
+    }
+    {
+        auto const [ai, bi] = m.compute_index(15, 15);
+        CHECK(ai == 3);
+        CHECK(bi == 63);
+    }
+}
+
+TEST_CASE("indexing", "[bit matrix]") {
+    constexpr flow::bit_matrix<2> m{};
+    static_assert(not m.index(0, 0));
+}
+
+TEST_CASE("set bit", "[bit matrix]") {
+    flow::bit_matrix<2> m{};
+    m.set(0, 0);
+    CHECK(m.index(0, 0));
+    CHECK(not m.index(0, 1));
+    CHECK(not m.index(1, 0));
+    CHECK(not m.index(1, 1));
+}
+
+TEST_CASE("set whole matrix", "[bit matrix]") {
+    flow::bit_matrix<8> m{};
+    m.set();
+    for (auto i = std::size_t{}; i < 8u; ++i) {
+        for (auto j = std::size_t{}; j < 8u; ++j) {
+            CHECK(m.index(i, j));
+        }
+    }
+}
+
+TEST_CASE("clear whole matrix", "[bit matrix]") {
+    flow::bit_matrix<16> m{};
+    m.set();
+    m.clear();
+    for (auto i = std::size_t{}; i < 16u; ++i) {
+        for (auto j = std::size_t{}; j < 16u; ++j) {
+            CHECK(not m.index(i, j));
+        }
+    }
+}
+
+TEST_CASE("equality", "[bit matrix]") {
+    auto m = flow::bit_matrix<12>{};
+    m.set();
+    auto n = flow::bit_matrix<12>{};
+    for (auto i = std::size_t{}; i < 12u; ++i) {
+        for (auto j = std::size_t{}; j < 12u; ++j) {
+            n.set(i, j);
+        }
+    }
+    CHECK(m == n);
+}
+
+TEST_CASE("identity matrix", "[bit matrix]") {
+    auto m = flow::bit_matrix<16>::identity();
+    CHECK(not m.index(0, 1));
+    CHECK(not m.index(1, 0));
+    for (auto i = std::size_t{}; i < 16u; ++i) {
+        CHECK(m.index(i, i));
+    }
+}
+
+TEST_CASE("create from string_view"
+          "[bit_matrix]") {
+    using namespace std::string_view_literals;
+    constexpr flow::bit_matrix<3> m{"100"
+                                    "010"
+                                    "001"sv};
+    static_assert(m == flow::bit_matrix<3>::identity());
+}
+
+TEST_CASE("or", "[bit matrix]") {
+    auto m = flow::bit_matrix<2>::identity();
+    auto n = flow::bit_matrix<2>{} | m;
+    CHECK(n == m);
+}
+
+TEST_CASE("and", "[bit matrix]") {
+    auto m = flow::bit_matrix<2>::identity();
+    auto n = flow::bit_matrix<2>{} & m;
+    CHECK(n == flow::bit_matrix<2>{});
+}
+
+TEST_CASE("complement", "[bit matrix]") {
+    auto m = ~flow::bit_matrix<2>::identity();
+    CHECK(not m.index(0, 0));
+    CHECK(m.index(0, 1));
+    CHECK(m.index(1, 0));
+    CHECK(not m.index(1, 1));
+}
+
+TEST_CASE("xor", "[bit matrix]") {
+    auto m = flow::bit_matrix<2>::identity();
+    auto n = m ^ m;
+    CHECK(n == flow::bit_matrix<2>{});
+}
+
+TEST_CASE("transpose identity", "[bit matrix]") {
+    auto m = flow::bit_matrix<2>::identity();
+    CHECK(transpose(m) == flow::bit_matrix<2>::identity());
+}
+
+TEST_CASE("transpose", "[bit matrix]") {
+    auto m = flow::bit_matrix<14>{};
+    m.set(0, 0);
+    m.set(12, 4);
+    m.set(13, 0);
+    auto n = flow::bit_matrix<14>{};
+    n.set(0, 0);
+    n.set(4, 12);
+    n.set(0, 13);
+    CHECK(transpose(m) == n);
+}
+
+TEST_CASE("add", "[bit matrix]") {
+    auto m = flow::bit_matrix<2>{};
+    m.set(0, 0);
+    auto n = flow::bit_matrix<2>{};
+    n.set(1, 1);
+    CHECK(m + n == flow::bit_matrix<2>::identity());
+}
+
+// TODO:
+// split into 4 submatrices for recursion
+
+// TEST_CASE("multiply", "[bit matrix]") {
+//     auto m = flow::bit_matrix<2>{};
+//     m.set(0, 0);
+//     m.set(1, 0);
+//     CHECK(m * m == m);
+// }
+
+// TEST_CASE("power", "[bit matrix]") {
+//     auto m = flow::bit_matrix<3>{};
+//     m.set(0, 0);
+//     m.set(0, 2);
+//     m.set(1, 2);
+//     m.set(2, 0);
+//     m.set(2, 1);
+
+//     auto n = flow::bit_matrix<3>{};
+//     n.set(0, 0);
+//     n.set(0, 1);
+//     n.set(0, 2);
+//     n.set(1, 0);
+//     n.set(1, 2);
+//     n.set(2, 0);
+//     n.set(2, 1);
+//     n.set(2, 2);
+//     CHECK(pow(m, 3) == n);
+// }
+
+// TEST_CASE("power (identity)", "[bit matrix]") {
+//     constexpr auto m = flow::bit_matrix<32>::identity();
+//     static_assert(pow(m, 32) == m);
+// }
